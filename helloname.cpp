2db50c548d41a9faa01184d51c5d572ede75d24e
Rowan Kasim <rowankasim@gmail.com>
2021-05-23 11:30:29 +0530

    Delete helloname.cpp

--- helloname.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    string name;
-    cout << "Please enter your name: " << endl;
-    cin >> name;
-    cout << "Hello, " << name << endl;
-}
