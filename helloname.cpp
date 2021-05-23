00b812fc534d7162dce247f8d01dba2e326acf4a
Rowan Kasim <rowankasim@gmail.com>
2021-05-23 11:19:54 +0530

    Add files via upload

---
+++ helloname.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string name;
+    cout << "Please enter your name: " << endl;
+    cin >> name;
+    cout << "Hello, " << name << endl;
+}
