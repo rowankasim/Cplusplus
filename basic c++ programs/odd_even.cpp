4344194a167f0560786ab3836ac41edbc11e1b46
Mohamed Kasim J <rowankasim25@gmail.com>
2021-05-23 17:53:23 +0530

    Update odd_even.cpp

--- basic c++ programs/odd_even.cpp
+++ basic c++ programs/odd_even.cpp
@@ -8,9 +8,18 @@ int main(int argc, const char **argv)
 {
     cout << "Welcome to the Odd or Even calculator\n";
     int number;
+    int condition;
+    do{
+    system("clear");
     cout << "Enter the number: \n";
     cin >> number;
     odd_even(number);
+    cout << "\nIf you want to continue enter '1' else '0'\n";
+    cin >> condition;
+    }while(condition);
+    if (condition ==0){
+        system("clear");
+    }
     return 0;
 }
 
@@ -18,10 +27,10 @@ void odd_even(int number)
 {
     if (number % 2 == 0)
     {
-        cout << "The entered number " << number << " is an Even Number" << endl;
+        cout << "The entered number " << number << " is an 'Even Number'" << endl;
     }
     else
     {
-        cout << "The entered number " << number << " is an Odd Number" << endl;
+        cout << "The entered number " << number << " is an 'Odd Number'" << endl;
     }
 }
