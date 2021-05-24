796533025b3b388d7a7e162489debef98dbcfcc0
Mohamed Kasim J <rowankasim@gmail.com>
2021-05-24 16:57:50 +0530

    Add files via upload

---
+++ basic c++ programs/create_and_write_a_file.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main(int argc, const char **argv)
+{
+    string name;
+    string filename;
+    int age, condition;
+    do
+    {
+        system("clear");
+        cout << "Welcome to the text file creating operation\n";
+        cout << "\nEnter your file name with proper extension: ";
+        cin >> filename;
+        cin.ignore();
+        ofstream outfile;
+        outfile.open(filename);
+        cout << "Text file is created successfully !\n";
+        cout << "\nEnter your name: ";
+        getline(cin, name);
+        cout << "Enter your age: ";
+        cin >> age;
+        cin.ignore();
+        outfile << "Name: " << name << endl;
+        outfile << "Age : " << age << endl;
+        cout << "\nIf you want to continue enter '1' else '0'\n";
+        cin >> condition;
+        cin.ignore();
+        if (condition == 0)
+        {
+            system("clear");
+        }
+    } while (condition);
+    return 0;
+}
