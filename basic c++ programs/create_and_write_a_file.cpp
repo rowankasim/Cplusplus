a5bdbca2e21df84a656ef530619d510b8271ccfc
Mohamed Kasim J <rowankasim25@gmail.com>
2021-05-24 19:14:03 +0530

    Update create_and_write_a_file.cpp

--- basic c++ programs/create_and_write_a_file.cpp
+++ basic c++ programs/create_and_write_a_file.cpp
@@ -5,32 +5,36 @@ int main(int argc, const char **argv)
 {
     string name;
     string filename;
-    int age, condition;
-    do
+    int count;
+    system("clear");
+    cout << "Welcome to the text file creating operation\n";
+    cout << "\nEnter your file name with proper extension: ";
+    cin >> filename;
+    ofstream outfile;
+    outfile.open(filename, ios::out);
+    if (!outfile.fail())
+    {
+        cout << "\nText file is created successfully !\n";
+    }
+    cout << "\nEnter the number of lines to be written: ";
+    cin >> count;
+    cin.ignore();
+    if (count != 0)
     {
         system("clear");
-        cout << "Welcome to the text file creating operation\n";
-        cout << "\nEnter your file name with proper extension: ";
-        cin >> filename;
-        cin.ignore();
-        ofstream outfile;
-        outfile.open(filename);
-        cout << "Text file is created successfully !\n";
-        cout << "\nEnter your name: ";
-        getline(cin, name);
-        cout << "Enter your age: ";
-        cin >> age;
-        cin.ignore();
-        outfile << "Name: " << name << endl;
-        outfile << "Age : " << age << endl;
-        outfile.close();
-        cout << "\nIf you want to continue enter '1' else '0'\n";
-        cin >> condition;
-        cin.ignore();
-        if (condition == 0)
+        cout << "\nWrite into your file :\n"
+             << endl;
+        for (int i = 1; i <= count; i++)
         {
-            system("clear");
+            getline(cin, name);
+            outfile << name << endl;
         }
-    } while (condition);
+        outfile.close();
+    }
+    else if (count == 0)
+    {
+        cout << "Inappropirate Input" << endl;
+    }
+
     return 0;
 }
